63a1c810c061bd37da603be4e723969b6079410b
rinat94ua <rinat94ua@gmail.com>
2018-09-09 23:50:11 +0300

    first commit

---
+++ src/MovieDetail.js
@@ -0,0 +1,40 @@
+/* eslint react/no-did-mount-set-state: 0 */
+
+import React, { Component } from 'react';
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+
+class MovieDetail extends Component {
+  state = {
+    movie: {},
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=5bcb07aa8f07d1e43144fd5fe96df5c9&language=en-US`);
+      const movie = await res.json();
+      this.setState({
+        movie,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    const { movie } = this.state;
+
+    return (
+      <div>
+        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
+        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        <h1>{movie.title}</h1>
+        <h3>{movie.release_date}</h3>
+        <p>{movie.overview}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieDetail;
---
+++ src/MoviesList.js
@@ -0,0 +1,35 @@
+/* eslint react/no-did-mount-set-state: 0 */
+
+import React, { Component } from 'react';
+
+import Movie from './Movie';
+
+class MoviesList extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=5bcb07aa8f07d1e43144fd5fe96df5c9&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <div>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
