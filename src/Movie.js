2fcb0975548c5ac35d139e0856f801b21b2bbda1
rinat94ua <rinat94ua@gmail.com>
2018-09-10 23:15:45 +0300

    mdb is finished

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
 import { Link } from 'react-router-dom';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
-    <Link to={`/${movie.id}`}>
+  <Link to={`/${movie.id}`}>
+    <Overdrive id={movie.id}>
       <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Link>
+    </Overdrive>
+  </Link>
 );
 
 export default Movie;
